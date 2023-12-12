e13b6bd11a344f90c2019b0ce0bd5e897f99fa59
A3aan4rie <adriaan.fourie77@outlook.com>
2023-12-12 19:54:49 +0200

    New Change

--- lab6b.cpp
+++ lab6b.cpp
@@ -33,7 +33,7 @@ int rollD4() {
 
 int main() {
 
-    // Seed the random number generator with the current time  -- added by AF
+    // Seed the random number generator with the current time  -- added by AFD
 
     srand(static_cast<unsigned int>(time(0)));
     
